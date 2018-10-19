01b3184c20ff3fec0f71bbbaa14c48a4d7e15478
NakiuS <jonatan.acahu@gmail.com>
2018-10-19 11:14:45 -0300

    The initial commint of project.

---
+++ ItemHelp - Editor/ItemHelp - Editor/BASE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace W2ItemHelp
+{
+    public partial class BASE
+    {
+        /// <summary>
+        /// Tamanho máximo de linhas permitida.
+        /// </summary>
+        public const int MAX_ITEMHELP_LENGTH =                10;
+    }
+}
---
+++ ItemHelp - Editor/ItemHelp - Editor/STRUCT_ITEMHELP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace W2ItemHelp
+{
+    public struct STRUCT_ITEMHELP
+    {
+        /// <summary>
+        /// Indício do item.
+        /// </summary>
+        public int Index;
+
+        public struct ST_HELPINFO
+        {
+            /// <summary>
+            /// Cor da linha.
+            /// </summary>
+            public int Color;
+
+            /// <summary>
+            /// Texto da linha.
+            /// </summary>
+            public string Message;
+        }
+
+        /// <summary>
+        /// Matriz que contém informações do item.
+        /// </summary>
+        public ST_HELPINFO[] Line;
+
+        /// <summary>
+        /// Método estático para criação de uma nova instância da estrutura.
+        /// </summary>
+        public static STRUCT_ITEMHELP CraftProperties()
+        {
+            var Help = new STRUCT_ITEMHELP();
+
+            Help.Line = new ST_HELPINFO[BASE.MAX_ITEMHELP_LENGTH];
+
+            return Help;
+        }
+    }
+}
