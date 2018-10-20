0bc0443bdd0a61de3029352f1d30b68320cd46f4
seu nome <seu email>
2018-10-20 11:48:21 -0300

    6

--- ItemHelp - Editor/ItemHelp - Editor/MainForm.cs
+++ ItemHelp - Editor/ItemHelp - Editor/MainForm.cs
@@ -166,7 +166,7 @@ namespace W2ItemHelp
           
         public static IEnumerable<T> GetFormControl<T>(Control _control) where T : Control
         {
-            var _CurControl = _control as T;
+            var _CurControl = _control as T; 
 
             if (_CurControl != null) yield return _CurControl;
 
